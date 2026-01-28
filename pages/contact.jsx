8944412d64d5586df4f6d6c95a48c02407a1a1e7
faris helmi <160445375+lancartekno@users.noreply.github.com>
2026-01-28 21:42:38 +0700

    komit

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -6,24 +6,24 @@ function Contact() {
 	return (
 		<>
 			<Head>
-			  <title>Contact</title>
+				<title>Contact</title>
 			</Head>
 
 			<Navbar />
 
 			<section className="hero">
-			  <div className="container">
-			    <div className="text-wrapper w-full">
-			      <h1 className="title">Contact</h1>
-			      <p className="description">
-			      	Berikut kontak yang dapat dihubungi terkait web developer.
-			      </p>
-			      <ul className="contact-links">
-			      	<li className="contact-item">Email: permanafarishelmi@gmail.com</li>
-			      	<li className="contact-item">Phone: (+62)831-3153-1052</li>
-			      </ul>
-		      </div>
-	      </div>
+				<div className="container">
+					<div className="text-wrapper w-full">
+						<h1 className="title">Contact</h1>
+						<p className="description">
+							Berikut kontak yang dapat dihubungi terkait web developer.
+						</p>
+						<ul className="contact-links">
+							<li className="contact-item">Email: permanafarishelmi@gmail.com</li>
+							<li className="contact-item">Whatsapp: <a href="https://wa.me/6283131531052" target="_blank" rel="noopener noreferrer">(+62)831-3153-1052</a></li>
+						</ul>
+					</div>
+				</div>
 			</section>
 		</>
 	);
